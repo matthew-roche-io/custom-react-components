0bbaf81b08a5cab4bdfe6a8550ee67f53c79f01d
Matthew Roche <mroche74@gmail.com>
2025-03-31 23:12:21 -0400

    chore: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { peerDependencies } from "./package.json";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "./src/index.ts", // Specifies the entry point for building the library.
+      name: "@matthew-roche-io/custom-react-components", // Sets the name of the generated library.
+      fileName: () => `index.js`, // Generates the output file name based on the format.
+      formats: ["es"], // Specifies the output formats (CommonJS and ES modules).
+    },
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies)], // Defines external dependencies for Rollup bundling.
+    },
+    sourcemap: true, // Generates source maps for debugging.
+    emptyOutDir: true, // Clears the output directory before building.
+  },
+  plugins: [dts()], // Uses the 'vite-plugin-dts' plugin for generating TypeScript declaration files (d.ts).
+});
